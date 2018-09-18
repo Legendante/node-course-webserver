cf77b98fef7c60227f274a8e54f002d41131fc1c
Legendante <jacques@legendante.com>
2018-09-18 14:00:31 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var retStr = `${now}: ${req.method} ${req.url}`;
+	console.log(retStr);
+	fs.appendFile('server.log', retStr + '\n', (err) => {
+		if(err)
+		{
+			console.log("Could not write to server.log");
+		}
+	});
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs', 
+	{
+		pageTitle: "Maintenance page",
+		welcomeMessage: "Welcome to Node JS server thingy"
+	});
+});
+
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', 
+	{
+		pageTitle: "Home page",
+		welcomeMessage: "Welcome to Node JS server thingy"
+	});
+});
+
+app.get('/about', (req, res) => {
+	// res.send('<h1>About Page</h1>');
+	res.render('about.hbs', 
+	{
+		pageTitle: "About page"
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send(
+	{
+	err: '<h1>Bad Browser!</h1>',
+	code: 'Cause I said so'
+	});
+});
+
+app.listen(3000, () => 
+{
+	console.log("Server is up on port 3000");
+});
