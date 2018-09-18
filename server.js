95ec9086f5e034180bfc6fab8beda6d594bba54d
Legendante <jacques@legendante.com>
2018-09-18 14:37:29 +0200

    Setup for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const heroport = process.env.PORT || 3000;  
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -60,7 +61,7 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => 
+app.listen(heroport, () => 
 {
-	console.log("Server is up on port 3000");
+	console.log(`Server is up on port ${heroport}`);
 });
